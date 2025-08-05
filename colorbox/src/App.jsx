e5b941bdb77796ba167b6b480ea0d0b69a76b173
Sneh Gurdasani <snehgurdasani1@outlook.com>
2025-08-05 16:03:34 +0530

    Add files via upload

---
+++ colorbox/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BoxContainer from './BoxContainer'
+
+function App() {
+  return (
+    <div className='App'>
+      <BoxContainer/>
+    </div>
+  );
+}
+
+export default App;
